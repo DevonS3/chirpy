adfd3e75d1baa396f3f2ac943719f48167eaeb03
DevonS3 <really_easy_to_remember@msn.com>
2025-07-28 21:37:43 -0700

    Added index.html

--- main.go
+++ main.go
@@ -6,10 +6,13 @@ import (
 
 func main() {
 	servMux := http.NewServeMux()
+	servMux.Handle("/", http.FileServer(http.Dir(".")))
+
 	var server http.Server
 
 	server.Handler = servMux
 	server.Addr = ":8080"
 
 	server.ListenAndServe()
+
 }
